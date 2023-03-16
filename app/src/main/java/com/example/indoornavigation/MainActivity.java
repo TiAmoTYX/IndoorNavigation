05f31fdfcd9d1ee14abe510525359c7988efd3d9
LST <1204276046@qq.com>
2023-03-16 11:38:36 +0800

    导航路线规划模块 起点、终点、路线

--- app/src/main/java/com/example/indoornavigation/MainActivity.java
+++ app/src/main/java/com/example/indoornavigation/MainActivity.java
@@ -4,6 +4,8 @@ package com.example.indoornavigation;
 
 import android.app.Activity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import android.view.View;
@@ -13,6 +15,13 @@ import com.example.indoornavigation.utils.FileUtils;
 import com.fengmap.android.FMDevice;
 import com.fengmap.android.FMErrorMsg;
 
+import com.fengmap.android.analysis.navi.FMNaviAnalyser;
+import com.fengmap.android.analysis.navi.FMNaviOption;
+import com.fengmap.android.analysis.navi.FMNavigation;
+import com.fengmap.android.analysis.navi.FMNavigationInfo;
+import com.fengmap.android.analysis.navi.FMPointOption;
+import com.fengmap.android.analysis.navi.FMSimulateNavigation;
+import com.fengmap.android.analysis.navi.OnFMNavigationListener;
 import com.fengmap.android.data.OnFMDownloadProgressListener;
 import com.fengmap.android.map.FMMap;
 import com.fengmap.android.map.FMMapUpgradeInfo;
@@ -20,12 +29,15 @@ import com.fengmap.android.map.FMMapView;
 import com.fengmap.android.map.animator.FMLinearInterpolator;
 import com.fengmap.android.map.event.OnFMMapInitListener;
 import com.fengmap.android.map.event.OnFMSwitchGroupListener;
+import com.fengmap.android.map.geometry.FMGeoCoord;
+import com.fengmap.android.map.geometry.FMMapCoord;
+import com.fengmap.android.utils.FMLog;
 import com.fengmap.android.widget.FMFloorControllerComponent;
 import com.fengmap.android.widget.FMMultiFloorControllerButton;
 import com.fengmap.android.widget.FMTextButton;
 
 
-public class MainActivity extends Activity implements OnFMMapInitListener, OnFMSwitchGroupListener{
+public class MainActivity extends Activity implements OnFMNavigationListener,OnFMMapInitListener, OnFMSwitchGroupListener{
     //地图
     FMMap mFMMap;
     private FMFloorControllerComponent mFloorComponent;
@@ -33,7 +45,34 @@ public class MainActivity extends Activity implements OnFMMapInitListener, OnFMS
     private FMMapView mMapView;
     //多层单层按钮
     FMMultiFloorControllerButton mMultiFloorBtn;
-    
+    // 导航对象
+    protected FMNavigation mNavigation;
+    // 导航配置
+    protected FMNaviOption mNaviOption;
+    // 点移距离视图中心点超过最大距离5米，就会触发移动动画
+    protected static final double NAVI_MOVE_CENTER_MAX_DISTANCE = 5;
+    // 进入导航时地图显示级别
+    protected static final int NAVI_ZOOM_LEVEL = 20;
+    // 起点标志物配置
+    protected FMPointOption mStartOption = new FMPointOption();
+    // 终点标志物配置
+    protected FMPointOption mEndOption = new FMPointOption();
+    // 路径是否计算完成
+    protected boolean isRouteCalculated;
+    // 地图是否加载完成
+    protected boolean isMapLoaded;
+    // 默认起点
+    protected FMGeoCoord mStartCoord = new FMGeoCoord(1,
+            new FMMapCoord(12613383.488510681, 2642576.1474729534));
+    // 默认终点
+    protected FMGeoCoord mEndCoord = new FMGeoCoord(3,
+            new FMMapCoord(12613464.576599307, 2642647.724973787));
+    // 是否为第一人称
+    private boolean mIsFirstView = true;
+    // 是否为跟随状态
+    private boolean mHasFollowed = true;
+    // 总共距离
+    private double mTotalDistance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,11 +102,12 @@ public class MainActivity extends Activity implements OnFMMapInitListener, OnFMS
      * 初始化多楼层控件位置
      */
     private void initMultiFloorControllerComponent() {
+        //创建多楼层显示按钮
         mMultiFloorBtn = new FMMultiFloorControllerButton(this);
+        //初始状态
         mMultiFloorBtn.initState(false);
         mMultiFloorBtn.measure(0, 0);
         int width = mMultiFloorBtn.getMeasuredWidth();
-
         //单/多层楼层切换控件
         mMapView.addComponent(mMultiFloorBtn, FMDevice.getDeviceWidth() - 10 - width, 60);
         //单、多楼层点击切换
@@ -143,7 +183,79 @@ public class MainActivity extends Activity implements OnFMMapInitListener, OnFMS
         if (mFloorComponent == null) {
             initFloorControllerComponent();
         }
+        // 创建模拟导航对象
+        mNavigation = new FMSimulateNavigation(mFMMap);
+        // 创建真实导航对象
+        // mNavigation = new FMActualNavigation(mFMMap);
+        // 设置导航文字语种，目前支持中文英文两种("zh"，"en")
+        // mNavigation.setNaviLanguage(this, "en");
+        // 设置是否启用走过导航线变化,默认开启
+        // mNavigation. setNaviAcrossChange(true);
+        // 创建导航配置对象
+        mNaviOption = new FMNaviOption();
+        // 设置跟随模式，默认跟随
+        mNaviOption.setFollowPosition(true);
+        // 设置跟随角度（第一人视角），默认跟随
+        mNaviOption.setFollowAngle(true);
+        // 点移距离视图中心点超过最大距离5米，就会触发移动动画；若设为0，则实时居中
+        mNaviOption.setNeedMoveToCenterMaxDistance(NAVI_MOVE_CENTER_MAX_DISTANCE);
+        // 设置导航开始时的缩放级别，true: 导航结束时恢复开始前的缩放级别，false：保持现状
+        mNaviOption.setZoomLevel(NAVI_ZOOM_LEVEL, false);
+        // 设置配置
+        mNavigation.setNaviOption(mNaviOption);
+        initNavi();
     }
+
+    private void initNavi() {
+        // 创建模拟导航对象
+        mNavigation = new FMSimulateNavigation(mFMMap);
+        // 设置导航文字语种，目前支持中文（CH）英文(EN)两种模式，不区分大小写，默认中文
+        mNavigation.setNaviLanguage(this, "Ch");
+        // 创建模拟导航配置对象
+        mNaviOption = new FMNaviOption();
+        // 设置跟随模式，默认跟随
+        mNaviOption.setFollowPosition(mHasFollowed);
+        // 设置跟随角度（第一人视角），默认跟随
+        mNaviOption.setFollowAngle(mIsFirstView);
+        // 点移距离视图中心点超过最大距离5米，就会触发移动动画；若设为0，则实时居中
+        mNaviOption.setNeedMoveToCenterMaxDistance(NAVI_MOVE_CENTER_MAX_DISTANCE);
+        // 设置导航开始时的缩放级别，true: 导航结束时恢复开始前的缩放级别，false：保持现状
+        mNaviOption.setZoomLevel(NAVI_ZOOM_LEVEL, false);
+        // 设置配置
+        mNavigation.setNaviOption(mNaviOption);
+        // 设置导航监听接口
+        mNavigation.setOnNavigationListener(this);
+        // 路径规划
+        analyzeNavigation(mStartCoord, mEndCoord);
+        // 总长
+        mTotalDistance = mNavigation.getSceneRouteLength();
+        isMapLoaded = true;
+    }
+
+    //导航
+    void analyzeNavigation(FMGeoCoord startPt, FMGeoCoord endPt) {
+        // 设置起点
+        Bitmap startBmp = BitmapFactory.decodeResource(getResources(), R.drawable.start);
+        mStartOption.setBitmap(startBmp);
+        mNavigation.setStartPoint(startPt);
+        mNavigation.setStartOption(mStartOption);
+
+        // 设置终点
+        Bitmap endBmp = BitmapFactory.decodeResource(getResources(), R.drawable.end);
+        mEndOption.setBitmap(endBmp);
+        mNavigation.setEndPoint(endPt);
+        mNavigation.setEndOption(mEndOption);
+
+        // 路径规划
+        int ret = mNavigation.analyseRoute(FMNaviAnalyser.FMNaviModule.MODULE_SHORTEST);
+        if (ret == FMNaviAnalyser.FMRouteCalcuResult.ROUTE_SUCCESS) {
+            mNavigation.drawNaviLine();
+            isRouteCalculated = true;
+        } else {
+            FMLog.le("failed",FMNaviAnalyser.FMRouteCalcuResult.getErrorMsg(ret));
+        }
+    }
+
 
     /**
      * 楼层切换控件初始化
@@ -225,4 +337,18 @@ public class MainActivity extends Activity implements OnFMMapInitListener, OnFMS
     }
 
 
+    @Override
+    public void onCrossGroupId(int i, int i1) {
+
+    }
+
+    @Override
+    public void onWalking(FMNavigationInfo fmNavigationInfo) {
+
+    }
+
+    @Override
+    public void onComplete() {
+
+    }
 }
